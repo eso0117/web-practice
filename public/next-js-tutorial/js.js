560d42c9aa15f6825b2bef78b6af7f516c12f8c9
Eso <eso0117@icloud.com>
2024-05-17 21:58:50 +0900

    Updated js for next-js tutorial

--- public/next-js-tutorial/js.js
+++ public/next-js-tutorial/js.js
@@ -1,3 +1,36 @@
-let aaaaa = "";
+let onAnswerClicked = (event) => {
+  const button = event.target;
+  const questionNumber = button.getAttribute("data-question");
+  const options = document.querySelectorAll(
+    `[data-question-number='${questionNumber}']`
+  );
 
-function a() {}
+  Array.prototype.forEach.call(options, (element) => {
+    element.classList.add("option-mark");
+  });
+};
+
+let onOptionClick = (event) => {
+  const target = event.target;
+  const questionNumber = target.getAttribute("data-question-number");
+
+  const options = document.querySelectorAll(
+    `[data-question-number='${questionNumber}']`
+  );
+
+  Array.prototype.forEach.call(options, (element) => {
+    element.classList.remove("option-selected");
+  });
+  target.classList.add("option-selected");
+};
+
+let checkAnswerElem = document.querySelectorAll("[data-js-check-answer]");
+let optionElem = document.querySelectorAll("[data-question-number]");
+
+Array.prototype.forEach.call(checkAnswerElem, (element) => {
+  element.onclick = onAnswerClicked;
+});
+
+Array.prototype.forEach.call(optionElem, (element) => {
+  element.onclick = onOptionClick;
+});
