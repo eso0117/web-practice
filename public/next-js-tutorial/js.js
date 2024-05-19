318c1a88fa4cfe6b051d0db3d642f9abda9efd4f
Eso <eso0117@icloud.com>
2024-05-19 21:34:18 +0900

    Updated js and css during chapter 3 documenting

--- public/next-js-tutorial/js.js
+++ public/next-js-tutorial/js.js
@@ -24,8 +24,37 @@ let onOptionClick = (event) => {
   target.classList.add("option-selected");
 };
 
+let onShowAndHideClicked = (event) => {
+  const button = event.currentTarget;
+  const buttonContent = $("[data-btn-content]", $(button));
+
+  const dataTarget = button.getAttribute("data-target");
+  const targetElems = document.querySelectorAll(
+    `[data-hide-target='${dataTarget}']`
+  );
+
+  const isHidden = button.classList.contains("show-and-hide-btn--hidden");
+
+  if (isHidden) {
+    button.classList.remove("show-and-hide-btn--hidden");
+    buttonContent.html("정답 숨기기");
+  } else {
+    button.classList.add("show-and-hide-btn--hidden");
+    buttonContent.html("정답 보이기");
+  }
+
+  Array.prototype.forEach.call(targetElems, (element) => {
+    if (isHidden) {
+      element.classList.remove("is-inactive");
+    } else {
+      element.classList.add("is-inactive");
+    }
+  });
+};
+
 let checkAnswerElem = document.querySelectorAll("[data-js-check-answer]");
 let optionElem = document.querySelectorAll("[data-question-number]");
+let showAndHideBtn = document.getElementsByClassName("show-and-hide-btn");
 
 Array.prototype.forEach.call(checkAnswerElem, (element) => {
   element.onclick = onAnswerClicked;
@@ -34,3 +63,8 @@ Array.prototype.forEach.call(checkAnswerElem, (element) => {
 Array.prototype.forEach.call(optionElem, (element) => {
   element.onclick = onOptionClick;
 });
+
+Array.prototype.forEach.call(showAndHideBtn, (element) => {
+  element.onclick = onShowAndHideClicked;
+});
+
